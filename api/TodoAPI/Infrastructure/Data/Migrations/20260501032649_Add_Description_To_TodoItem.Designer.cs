3c9010dbcd8fa27be331dd65f5c87bf619609603
Arthur Cruz <arthurcruzdbp@gmail.com>
2026-05-01 00:27:21 -0300

    fix

--- api/TodoAPI/Infrastructure/Data/Migrations/20260501032649_Add_Description_To_TodoItem.Designer.cs
+++ api/TodoAPI/Infrastructure/Data/Migrations/20260501032649_Add_Description_To_TodoItem.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TodoAPI.Infrastructure.Data;
@@ -11,9 +12,11 @@ using TodoAPI.Infrastructure.Data;
 namespace TodoAPI.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20260501032649_Add_Description_To_TodoItem")]
+    partial class Add_Description_To_TodoItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ api/TodoAPI/Infrastructure/Data/Migrations/20260501032649_Add_Description_To_TodoItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoAPI.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Description_To_TodoItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "tasks",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "tasks");
+        }
+    }
+}
